f4df66d97eb98e1939d760b7ff39a12f2caea67e
jooyeongmee <89931104+jooyeongmee@users.noreply.github.com>
2023-02-05 03:00:14 +0900

    [Bronze V] Title: 영수증, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,13 @@
+total = int(input())
+num = int(input())
+sum = 0
+
+for i in range(num):
+    price, n = map(int, input().split())
+
+    sum += price * n
+
+if (sum == total):
+    print("Yes")
+else:
+    print("No")
