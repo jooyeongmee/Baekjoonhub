699f7dfdc04e694d32973847f1fbf54bc0a8beec
jooyeongmee <89931104+jooyeongmee@users.noreply.github.com>
2023-02-21 19:01:05 +0900

    [Silver IV] Title: 큐 2, Time: 1696 ms, Memory: 175148 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+
+N = int(input())
+queue = deque()
+for i in range(N):
+  cmd = sys.stdin.readline().split()
+  if cmd[0] == "pop":
+    if len(queue) == 0: 
+      print(-1)
+    else:
+      print(queue.popleft()) 
+  elif cmd[0] == "size":
+    print(len(queue))
+  elif cmd[0] == "empty":
+    if (len(queue) == 0): print(1)
+    else: print(0)
+  elif cmd[0] == "front":
+    if (len(queue) == 0): print(-1)
+    else: print(queue[0])
+  elif cmd[0] == "back":
+    if (len(queue) == 0): print(-1)
+    else: print(queue[-1])
+  else:
+    arg = cmd[1]
+    queue.append(arg)
