ce73e2d0c28e211f23d7fcfa6ad0c5b3d1ad41fe
YINGMEI <j.yeongmee@gmail.com>
2023-03-16 13:48:15 +0900

    [Gold V] 동전

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,19 @@
+import sys
+
+t = int(input())
+for _ in range(t):
+    n = int(sys.stdin.readline())
+    coins = list(map(int, sys.stdin.readline().split()))
+    m = int(sys.stdin.readline())
+
+    dp = [0] * (m+1)
+    dp[0] = 1
+    for coin in coins:
+        for i in range(m+1):
+            if i >= coin:
+                dp[i] += dp[i-coin]
+
+    print(dp[m])
+
+
+
