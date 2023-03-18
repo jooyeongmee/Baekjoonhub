90eebd12518b65bc9f3d9b3935922a3864efef26
jooyeongmee <89931104+jooyeongmee@users.noreply.github.com>
2023-03-18 22:31:41 +0900

    [Silver I] Title: 징검다리 건너기, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/21317. 징검다리 건너기/징검다리 건너기.py
+++ 백준/Silver/21317. 징검다리 건너기/징검다리 건너기.py
@@ -3,11 +3,13 @@ import sys
 n = int(sys.stdin.readline().strip())
 val = 9999999999
 stones = []
+# stones_tu = []
 dp = [0] + [val] * (n-1)
 
 for i in range(n-1):
     s, b = map(int, sys.stdin.readline().strip().split())
-    stones.append([s, b])
+    stones.append((s, b))
+    # stones_tu.append((s, b))
 
     if i + 1 < n:
         dp[i+1] = min(dp[i+1], dp[i]+s)
@@ -21,8 +23,10 @@ for i in range(3, n):
     energy, dp1, dp2 = dp[i-3]+k, val, val
     for j in range(i, n-1):
         if i + 1 <= n:
+            # print("0: " + str(stones[j][0] == stones_tu[j][0]))
             dp1 = min(dp1, energy + stones[j][0])
         if i + 2 <= n:
+            # print("1: " + str(stones[j][1] == stones_tu[j][1]))
             dp2 = min(dp2, energy + stones[j][1])
         energy, dp1, dp2 = dp1, dp2, val
     min_energy = min(min_energy, energy)
