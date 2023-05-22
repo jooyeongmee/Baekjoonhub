8a99e12778c1c045d0016fe770aafed8c15b46f9
jooyeongmee <89931104+jooyeongmee@users.noreply.github.com>
2023-05-22 16:08:46 +0900

    [Silver III] Title: 파일 정리, Time: 216 ms, Memory: 41544 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -3,12 +3,15 @@ import sys
 num = int(input())
 
 files = {}
+file_list = []
 for i in range(num):
     name, ext = sys.stdin.readline().rstrip().split('.')
     if ext in files.keys():
         files[ext] += 1
     else:
         files[ext] = 1
+        file_list.append(ext)
 
-for key, value in sorted(files.items()):
-    print(key, str(value))
+file_list.sort()
+for _ in file_list:
+    print(_, files[_])
