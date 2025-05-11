6d60a95bcb5a015baa405534e3335c4a3f1a7a12
YINGMEI ZHU <89931104+jooyeongmee@users.noreply.github.com>
2025-05-11 17:12:07 +0900

    [Bronze III] Title: 약수 구하기, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -1,19 +1,11 @@
-import sys
-
-input = sys.stdin.readline
-
 n, k = map(int, input().split())
-result = set()
 
+count = 0
 for i in range(1, n+1):
     if n % i == 0:
-        result.add(n // i)
-
-result = list(result)
-result.sort()
-
-if len(result) >= k:
-    print(result[k-1])
-else:
-    print(0)
+        count += 1
+    if count == k:
+        print(i)
+        exit()
     
+print(0)
