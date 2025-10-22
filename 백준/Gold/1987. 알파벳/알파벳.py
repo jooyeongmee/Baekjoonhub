33fa358c4dd487134fee16455f49ce1f4724eaf2
YINGMEI ZHU <89931104+jooyeongmee@users.noreply.github.com>
2025-10-22 15:59:16 +0900

    [Gold IV] Title: 알파벳, Time: 5188 ms, Memory: 145460 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 
 r, c = map(int, input().split())
-graph = [list(input().strip()) for _ in range(r)]
+graph = [list(map(lambda x: ord(x) - ord('A'), input().strip())) for _ in range(r)]
 visited_alphabets = [False] * 26
 
 max_count = 0
@@ -11,20 +11,20 @@ dy = [0, 1, 0, -1]
 dx = [1, 0, -1, 0]
 
 def dfs(y, x):
-    global max_count, count, visited_alphabets
+    global max_count, count
     count += 1
     for k in range(4):
         ny = y + dy[k]
         nx = x + dx[k]
         if  0<= ny < r and 0<= nx < c:
-            if visited_alphabets[ord(graph[ny][nx]) - ord('A')] == False:
-                visited_alphabets[ord(graph[ny][nx]) - ord('A')] = True
+            if visited_alphabets[graph[ny][nx]] == False:
+                visited_alphabets[graph[ny][nx]] = True
                 dfs(ny, nx)
     max_count = max(max_count, count)
-    visited_alphabets[ord(graph[y][x]) - ord('A')] = False
+    visited_alphabets[graph[y][x]] = False
     count -= 1  
 
-visited_alphabets[ord(graph[0][0]) - ord('A')] = True
+visited_alphabets[graph[0][0]] = True
 dfs(0, 0)
 print(max_count)
 
