5f0827b972493b46ab99d2f03cf668f09175b90c
YINGMEI ZHU <89931104+jooyeongmee@users.noreply.github.com>
2025-10-22 14:53:53 +0900

    [Silver I] Title: 단지번호붙이기, Time: 32 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,43 +1,38 @@
-from collections import deque
 import sys
-
 input = sys.stdin.readline
 
-n = int(input().rstrip())
-
-data = [list(map(int, input().rstrip())) for _ in range(n)]
+n = int(input())
+graph = [list(map(int, input().strip())) for _ in range(n)]
 check = [[False] * n for _ in range(n)]
+
+each = 0
+result = []
 
 dy = [0, 1, 0, -1]
 dx = [1, 0, -1, 0]
 
-answer = []
-
-def bfs(y, x):
-    rs = 1
-    queue = deque()
-    queue.append((y, x))
-    while queue:
-        ey, ex = queue.popleft()
-        for k in range(4):
-            ny = ey + dy[k]
-            nx = ex + dx[k]
-            if 0 <= ny < n and 0 <= nx < n:
-                if data[ny][nx] == 1 and check[ny][nx] == False:
-                    rs += 1
-                    check[ny][nx] = True
-                    queue.append((ny, nx))
-
-    return rs
-
-
+def dfs(y, x):
+    global each
+    each += 1
+    for k in range(4):
+        ny = y + dy[k]
+        nx = x + dx[k]
+        if 0 <= ny < n and 0 <= nx < n:
+            if graph[ny][nx] == 1 and not check[ny][nx]:
+                check[ny][nx] = True
+                dfs(ny, nx)
+                
+                
 for j in range(n):
     for i in range(n):
-        if data[j][i] == 1 and check[j][i] == False:
+        if graph[j][i] == 1 and not check[j][i]:
             check[j][i] = True
-            answer.append(bfs(j, i))
+            each = 0
+            dfs(j, i)
+            result.append(each)
 
-print(len(answer))
-answer.sort()
-for i in range(len(answer)):
-    print(answer[i])
+result.sort()
+print(len(result))
+
+for r in result:
+    print(r)
